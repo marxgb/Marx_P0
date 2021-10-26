d43a7ab95db138941f16414c0912d4662381f2a9
marxb <marxbacungan@gmail.com>
2021-10-25 22:29:39 -0400

    added user parameter and enable to check workouts for a user

--- src/main/scala/WorkoutLog.scala
+++ src/main/scala/WorkoutLog.scala
@@ -26,66 +26,84 @@ object WorkoutLog {
 
     //GET USER
     println("Enter your user:")
-    var userN = scala.io.StdIn.readLine()
+    var user = scala.io.StdIn.readLine()
 
 
     //VARIABLES
     var logWorkout = true
     var workoutDate = ""
+    var option = ""
 
-    println("Press 1 to log a workout, 2 to 3")
-    var option = scala.io.StdIn.readLine()
 
     //FOR WHEN THE USER WANTS TO LOG A WORKOUT
-    while (logWorkout == true && option == "1"){
-      if(workoutDate == ""){
-      println("What date is your workout? (yyyy-mm-dd format)")
-      workoutDate = scala.io.StdIn.readLine()
+    while (logWorkout == true){
+      println("Press 1 to log a workout, 2 to check user workout logs")
+      option = scala.io.StdIn.readLine()
+      while(option =="1") {
+        if (workoutDate == "") {
+          println("What date is your workout? (yyyy-mm-dd format)")
+          workoutDate = scala.io.StdIn.readLine()
+        }
+        println("What category workout do you want to log? chest legs arms back")
+        var workoutGroup = scala.io.StdIn.readLine()
+        var workoutName = checkCategory(workoutGroup)
+        createWorkoutRecord(workoutDate, workoutName, connection, user)
+        println("Your workout has been success fully log! Do you wanna log more workout? y or n")
+        var continueLog = scala.io.StdIn.readLine()
+        if (continueLog == "n")
+          option = "0"
+        else if (continueLog == "y")
+          print("Logging more workouts. ")
+        else
+          println("invalid command. Do you wanna log more workout? y or n")
       }
-      println("What category workout do you want to log? chest legs arms back")
-      var workoutGroup = scala.io.StdIn.readLine()
-      var workoutName = checkCategory(workoutGroup)
-      createWorkoutRecord(workoutDate, workoutName, connection)
-      println("Your workout has been success fully log! Do you wanna log more workout? y or n")
-      var continueLog = scala.io.StdIn.readLine()
-      if (continueLog == "n")
+      if (option == "2"){
+        checkUser(user, connection)
         logWorkout = false
-      else if (continueLog == "y")
-        print("Logging more workouts. ")
-      else
-        println("invalid command. Do you wanna log more workout? y or n")
-      println("THANK YOU FOR LOGGING YOUR WORKOUT!!")
+      }
+
     }
+
+    println("THANK YOU FOR LOGGING YOUR WORKOUT!!")
   }
 
   //END OF MAIN
   //LIST OF METHODS
-  def checkUser(x : String, y : String): Unit = {
-    println(x + " " + y)
+  def checkUser(x : String, connection: Connection): Unit = {
+    val statement = connection.createStatement
+    var pstmt = connection.prepareStatement("SELECT * FROM workouts WHERE user=?")
+    pstmt.setString(1,x)
+    var rs = pstmt.executeQuery()
+    while (rs.next) {
+      val workoutDate = rs.getDate("workoutDate")
+      val workoutName= rs.getString("workoutName")
+      val workoutWeight = rs.getInt("workoutWeight")
+      val workoutSet = rs.getInt("workoutSet")
+      val workoutRep = rs.getInt("workoutRep")
+      val user = rs.getString("user")
+      println("Date: %s | Workout: %s | Weight: %s lbs | Set: %s | Rep: %s | User: %s".format(workoutDate, workoutName, workoutWeight, workoutSet, workoutRep, user))
+    }
+
   }
 
   //METHOD THAT INSERTS A RECORD INTO THE WORKOUTS TABLE
-  def logWorkoutDB(date : String, name : String, weight : String, rep : String, set: String, connection: Connection): Unit ={
+  def logWorkoutDB(date : String, name : String, weight : String, rep : String, set: String, connection: Connection, user : String): Unit ={
     var statement = connection.createStatement
-    /*
-      var pstmt = "INSERT INTO workouts(workoutDate,workoutName,workoutWeight,workoutSet,workoutRep) VALUES(STR_TO_DATE('10-14-2021','%d-%m-%Y'),'Bench Press',185,5,5)"
-      statement.executeUpdate(pstmt)
-      statement.executeUpdate(pstmt)
-    */
-      var pstmt = connection.prepareStatement("INSERT INTO workouts(workoutDate,workoutName,workoutWeight,workoutSet,workoutRep) VALUES(?,?,?,?,?)")
-      pstmt.setDate(1, java.sql.Date.valueOf(date))
-      pstmt.setString(2, name)
-      pstmt.setInt(3, weight.toInt)
-      pstmt.setInt(4, rep.toInt)
-      pstmt.setInt(5, set.toInt)
-      pstmt.executeUpdate
+    var pstmt = connection.prepareStatement("INSERT INTO workouts(workoutDate,workoutName,workoutWeight,workoutSet,workoutRep, user) VALUES(?,?,?,?,?,?)")
+    pstmt.setDate(1, java.sql.Date.valueOf(date))
+    pstmt.setString(2, name)
+    pstmt.setInt(3, weight.toInt)
+    pstmt.setInt(4, rep.toInt)
+    pstmt.setInt(5, set.toInt)
+    pstmt.setString(6, user)
+    pstmt.executeUpdate
   }
 
   //METHOD THAT CHECK FOR CATEGORY AND CORRECT WORKOUT NAME
   def checkCategory(x: String): String = {
     var workoutGroup = x
     var workoutName = ""
-    val workoutGroupList = List("chest", "legs","arms","legs")
+    val workoutGroupList = List("chest", "legs","arms","back")
     val workoutList = List("bench press", "incline press","dumbbell flies", "squat","leg press", "leg curl", "bicep curl", "tricep extension", "hammer curl", "deadlift", " pull downs", "rows")
     while(!workoutGroupList.contains(workoutGroup)){
       println("Invalid category. Please select a valid category: 'chest' 'legs' 'arms' legs'")
@@ -105,15 +123,16 @@ object WorkoutLog {
     while(!workoutList.contains(workoutName))
     workoutName
   }
+
   //TAKES THE INSERT VALUES FOR SET/REPS/WEIGHT
-  def createWorkoutRecord(workoutDate : String, workoutName: String, connection: Connection): Unit = {
+  def createWorkoutRecord(workoutDate : String, workoutName: String, connection: Connection, user: String): Unit = {
     println("How many lbs was your set?")
     var workoutWeight = scala.io.StdIn.readLine()
     println("How many sets did you do?")
     var workoutSet = scala.io.StdIn.readLine()
     println("How many reps did you do per set?")
     var workoutRep = scala.io.StdIn.readLine()
-    logWorkoutDB(workoutDate, workoutName, workoutWeight, workoutSet, workoutRep, connection)
+    logWorkoutDB(workoutDate, workoutName, workoutWeight, workoutSet, workoutRep, connection, user)
 
   }
 
